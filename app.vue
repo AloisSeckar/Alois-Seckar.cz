839569182535ad1580ee3b08f94cda156101b282
Alois Sečkár <ellrohir@seznam.cz>
2022-11-02 22:11:10 +0100

    some basic styling

--- app.vue
+++ app.vue
@@ -1,7 +1,40 @@
 <template>
   <div>
+    <img src="@/as.webp" />
     <h1>Alois Sečkár</h1>
     <p>Programátor / Běžec / Politik</p>
-    <p>Auto-deployment to Netlify</p>
+    <p>&copy; 2022</p>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "Alois-seckar.cz",
+  bodyAttrs: {
+    style: 'background-color: #009682;'
+  }
+})
+</script>
+
+<style scoped>
+@font-face {
+  font-family: kaushan;
+  src: url('~/assets/KaushanScript-AS.woff2');
+}
+
+div {
+  text-align: center;
+  color: white;
+  padding-top: 10%;
+}
+
+img {
+  border-radius: 50%;
+  height: 200px;
+}
+
+h1 {
+  font-family: kaushan, cursive;
+  font-size: 50px;
+}
+</style>
