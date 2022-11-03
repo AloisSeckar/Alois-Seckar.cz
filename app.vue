6dc6e015c2a175c45bff826dde2be54abd68eccc
Alois Sečkár <ellrohir@seznam.cz>
2022-11-03 20:44:27 +0100

    social media + footer

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <img src="/as.webp" />
+    <img class="logo" src="/as.webp" alt="AS" title="Alois Sečkár" />
     <h1>Alois Sečkár</h1>
-    <p>Programátor / Běžec / Politik</p>
-    <p>&copy; 2022</p>
+    <p>Programátor / Běžec / Patriot</p>
+    <a href="https://www.facebook.com/alois.seckar"><img class="social" src="/fb.webp" alt="FB" title="Facebook" /></a>
+    <a href="https://www.instagram.com/alois.seckar/"><img class="social" src="/ig.webp" alt="IG" title="Instagram" /></a>
+    <a href="https://twitter.com/AloisSeckar/"><img class="social" src="/tw.webp" alt="TW" title="Twitter" /></a>
+    <a href="https://www.linkedin.com/in/alois-/"><img class="social" src="/ln.webp" alt="LN" title="LinkedIn" /></a>
+    <div id="footer">
+      <hr />
+      <p><strong>&copy; 2022 Alois Sečkár</strong> | <a href="https://www.vecteezy.com/free-vector/social-media">Social Media Vectors by Vecteezy</a></p>
+    </div>  
   </div>
 </template>
 
@@ -25,16 +32,32 @@ useHead({
 div {
   text-align: center;
   color: white;
-  padding-top: 10%;
+  padding-top: 5%;
 }
 
-img {
+img.logo {
   border-radius: 50%;
   height: 200px;
+}
+
+img.social {
+  height: 32px;
+  margin: 4px;
 }
 
 h1 {
   font-family: kaushan, cursive;
   font-size: 50px;
 }
+
+a:link, a:hover, a:active, a:visited {
+  color: white;
+}
+
+#footer {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+}
+
 </style>
