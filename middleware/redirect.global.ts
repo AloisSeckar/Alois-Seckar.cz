34b0ce4d82f80848bc5ba37180d1d1c52739e4ae
Alois Sečkár <ellrohir@seznam.cz>
2022-11-14 23:26:22 +0100

    redirect fix

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    switch (to.path) {
-        // in case users come from old url
-        case '/ramecky/index.php':
-            return navigateTo('/ramecky')
+    // in case users come from old url
+    if (to.path?.includes('/ramecky/index.php')) {
+        return navigateTo('/ramecky')
     }
 })
