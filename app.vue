0b7fe1d2eb556a56e34d30d7f3a068abb4883eb6
Alois Sečkár <ellrohir@seznam.cz>
2023-11-21 18:49:27 +0100

    feat: html meta description

--- app.vue
+++ app.vue
@@ -7,6 +7,9 @@
 <script setup lang="ts">
 useHead({
   title: 'Alois-seckar.cz',
+  meta: [
+    { name: 'description', content: 'Personal page of Alois Sečkár, Prague, Czechia' }
+  ],
   htmlAttrs: {
     lang: 'cs'
   }
