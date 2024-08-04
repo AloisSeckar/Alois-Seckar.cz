7d954ade4233ecaa16fc003778f2d42d3c9bb7a0
Alois Sečkár <ellrohir@seznam.cz>
2024-08-04 22:30:00 +0200

    build: nuxt compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,26 @@
 export default defineNuxtConfig({
+  
+  compatibilityDate: '2024-08-04',
+
   css: [
     '@/assets/as.css',
   ],
+
   modules: [
     '@nuxt/eslint',
     'nuxt-security',
   ],
+
   eslint: {
     config: {
       stylistic: true,
     },
   },
+
   security: {
     corsHandler: {
       origin: '*',
     },
   },
+
 })
