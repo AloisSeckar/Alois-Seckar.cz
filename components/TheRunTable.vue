60b2b530004e2ca4fc41c6ab39bc8a901f545679
Alois Sečkár <ellrohir@seznam.cz>
2024-12-31 20:31:14 +0100

    feat: run stats

--- components/TheRunTable.vue
+++ components/TheRunTable.vue
@@ -4,6 +4,43 @@
       Načítání...
     </div>
     <ClientOnly>
+      <div class="mx-auto my-4 grid grid-cols-3 min-[400px]:grid-cols-4 min-[860px]:grid-cols-7 gap-4 text-sm text-black justify-center">
+        <div :class="`${statsCircleClass} bg-yellow-300`" title="Uběhnuté kilometry v aktuálním měsíci">
+          {{ aktualniMesic }}/{{ aktualniRok }}
+          <br>
+          {{ kmZaAktualniMesic }}  km
+        </div>
+        <div :class="`${statsCircleClass} bg-yellow-300`" title="Uběhnuté kilometry v aktuálním roce">
+          {{ aktualniRok }}
+          <br>
+          {{ kmZaAktualniRok }} km
+        </div>
+        <div :class="`${statsCircleClass} bg-green-300`" title="Nejvíc kilometrů za jeden měsíc">
+          {{ nejlepsiMesic }}
+          <br>
+          {{ nejlepsiMesicKm }}
+        </div>
+        <div :class="`${statsCircleClass} bg-green-300`" title="Nejvíc kilometrů za jeden rok">
+          {{ nejlepsiRok }}
+          <br>
+          {{ nejepsiRokKm }} km
+        </div>
+        <div :class="`${statsCircleClass} bg-sky-300`" title="Průměrně v tomto měsíci (od 2013)">
+          ⌀ Měsíc
+          <br>
+          {{ prumerZaMesic }} km
+        </div>
+        <div :class="`${statsCircleClass} bg-sky-300`" title="Průměrně v jednom roce (od 2013)">
+          ⌀ Rok
+          <br>
+          {{ prumerZaRok }} km
+        </div>
+        <div :class="`${statsCircleClass} bg-red-300 hidden min-[400px]:flex`" title="Celkem uběhnutých kilometrů (od 2013)">
+          Celkem
+          <br>
+          <span class="text-xs">{{ getCelkem() }} km</span>
+        </div>
+      </div>
       <UTable :rows :columns :ui>
         <template #rdate-data="{ row }: TableData">
           {{ useDateFormat(row.rdate, 'DD.MM.YYYY') }}
@@ -28,6 +65,8 @@
 </template>
 
 <script setup lang="ts">
+const statsCircleClass = 'w-24 h-24 flex flex-col justify-center rounded-full'
+
 // customize UTable
 const columns = [{
   key: 'rdate',
@@ -112,5 +151,96 @@ function getLengthInfo(row: RunRecord) {
 function parseTimeInfo(time: string) {
   const index = time.lastIndexOf(':')
   return time.slice(0, index) + '.' + time.slice(index + 1)
+}
+
+const aktualniDatum = new Date().toISOString()
+const aktualniRok = aktualniDatum.slice(0, 4)
+const aktualniMesic = aktualniDatum.slice(5, 7)
+
+const kmZaAktualniMesic = getCelkemZaObdobi(aktualniRok, aktualniMesic)
+const kmZaAktualniRok = getCelkemZaObdobi(aktualniRok)
+
+const kmZaKazdyRok = {}
+const kmZaKazdyMesic = {}
+data.value?.forEach((run) => {
+  const runRok = useDateFormat(run.rdate, 'YYYY').value
+  const runMesic = useDateFormat(run.rdate, 'YYYY-MM').value
+
+  if (!kmZaKazdyRok[runRok]) {
+    kmZaKazdyRok[runRok] = 0
+  }
+  kmZaKazdyRok[runRok] += run.tid > 0 ? run.tlength : run.rlength
+
+  if (!kmZaKazdyMesic[runMesic]) {
+    kmZaKazdyMesic[runMesic] = 0
+  }
+  kmZaKazdyMesic[runMesic] += run.tid > 0 ? run.tlength : run.rlength
+})
+
+let nejlepsiRok = ''
+let maxZaRok = 0
+for (const rok in kmZaKazdyRok) {
+  if (kmZaKazdyRok[rok] > maxZaRok) {
+    maxZaRok = kmZaKazdyRok[rok]
+    nejlepsiRok = rok
+  }
+}
+const nejepsiRokKm = (maxZaRok / 1000).toFixed(1)
+
+let nejlepsiMesic = ''
+let maxZaMesic = 0
+for (const mesic in kmZaKazdyMesic) {
+  if (kmZaKazdyMesic[mesic] > maxZaMesic) {
+    maxZaMesic = kmZaKazdyMesic[mesic]
+    nejlepsiMesic = mesic
+  }
+}
+nejlepsiMesic = nejlepsiMesic.split('-').reverse().join('/')
+const nejlepsiMesicKm = (maxZaMesic / 1000).toFixed(1)
+
+function getCelkemZaObdobi(rok: string, mesic?: string) {
+  let totalYear = 0
+  data.value?.forEach((run) => {
+    let runCounts = false
+    const runRok = useDateFormat(run.rdate, 'YYYY').value
+    if (runRok === rok) {
+      if (mesic) {
+        const runMesic = useDateFormat(run.rdate, 'MM').value
+        runCounts = runMesic === mesic
+      } else {
+        runCounts = true
+      }
+    }
+    if (runCounts) {
+      totalYear += run.tid > 0 ? run.tlength : run.rlength
+    }
+  })
+  return (totalYear / 1000).toFixed(1)
+}
+
+let celkemZaVsechnyRoky = 0
+let pocetLet = 0
+for (const year in kmZaKazdyRok) {
+  celkemZaVsechnyRoky += kmZaKazdyRok[year]
+  pocetLet++
+}
+const prumerZaRok = ((celkemZaVsechnyRoky / pocetLet) / 1000).toFixed(1)
+
+let celkemZaVsechnyMesice = 0
+let pocetMesicu = 0
+for (const mesic in kmZaKazdyMesic) {
+  if (mesic.endsWith(aktualniMesic)) {
+    celkemZaVsechnyMesice += kmZaKazdyMesic[mesic]
+    pocetMesicu++
+  }
+}
+const prumerZaMesic = ((celkemZaVsechnyMesice / pocetMesicu) / 1000).toFixed(1)
+
+function getCelkem() {
+  let totalMeters = 0
+  data.value?.forEach((run) => {
+    totalMeters += run.tid > 0 ? run.tlength : run.rlength
+  })
+  return (totalMeters / 1000).toFixed(1)
 }
 </script>
