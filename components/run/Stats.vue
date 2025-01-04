618e2537918ef996f5776824489c27c1579a7530
Alois Sečkár <ellrohir@seznam.cz>
2025-01-04 16:09:56 +0100

    fix: stats update reactively

--- components/run/Stats.vue
+++ components/run/Stats.vue
@@ -18,7 +18,7 @@
     <div :class="`${statsCircleClass} bg-green-300`" title="Nejvíc kilometrů za jeden rok">
       {{ nejlepsiRok }}
       <br>
-      {{ nejepsiRokKm }} km
+      {{ nejlepsiRokKm }} km
     </div>
     <div :class="`${statsCircleClass} bg-sky-300`" title="Průměrně v tomto měsíci (od 2013)">
       ⌀ Měsíc
@@ -33,7 +33,7 @@
     <div :class="`${statsCircleClass} bg-red-300 hidden min-[400px]:flex`" title="Celkem uběhnutých kilometrů (od 2013)">
       Celkem
       <br>
-      <span class="text-xs">{{ getCelkem() }} km</span>
+      <span class="text-xs">{{ celkemKm }} km</span>
     </div>
   </div>
 </template>
@@ -49,46 +49,93 @@ const aktualniDatum = new Date().toISOString()
 const aktualniRok = aktualniDatum.slice(0, 4)
 const aktualniMesic = aktualniDatum.slice(5, 7)
 
-const kmZaAktualniMesic = getCelkemZaObdobi(aktualniRok, aktualniMesic)
-const kmZaAktualniRok = getCelkemZaObdobi(aktualniRok)
+const kmZaAktualniMesic = ref('0')
+const kmZaAktualniRok = ref('0')
+const nejlepsiMesic = ref('0')
+const nejlepsiMesicKm = ref('0')
+const nejlepsiRok = ref('0')
+const nejlepsiRokKm = ref('0')
+const prumerZaMesic = ref('0')
+const prumerZaRok = ref('0')
 
-const kmZaKazdyRok = {} as RunStats
-const kmZaKazdyMesic = {} as RunStats
-runs.forEach((run) => {
-  const runRok = useDateFormat(run.rdate, 'YYYY').value
-  const runMesic = useDateFormat(run.rdate, 'YYYY-MM').value
-
-  if (!kmZaKazdyRok[runRok]) {
-    kmZaKazdyRok[runRok] = 0
-  }
-  kmZaKazdyRok[runRok] += run.tid > 0 ? run.tlength : run.rlength
-
-  if (!kmZaKazdyMesic[runMesic]) {
-    kmZaKazdyMesic[runMesic] = 0
-  }
-  kmZaKazdyMesic[runMesic] += run.tid > 0 ? run.tlength : run.rlength
+const celkemKm = computed(() => {
+  let totalMeters = 0
+  runs.forEach((run) => {
+    totalMeters += run.tid > 0 ? run.tlength : run.rlength
+  })
+  return (totalMeters / 1000).toFixed(1)
 })
+watch(celkemKm, () => {
+  console.log('celkem km changed to ' + celkemKm.value)
 
-let nejlepsiRok = ''
-let maxZaRok = 0
-for (const rok in kmZaKazdyRok) {
-  if (kmZaKazdyRok[rok] > maxZaRok) {
-    maxZaRok = kmZaKazdyRok[rok]
-    nejlepsiRok = rok
+  kmZaAktualniMesic.value = getCelkemZaObdobi(aktualniRok, aktualniMesic)
+  kmZaAktualniRok.value = getCelkemZaObdobi(aktualniRok)
+
+  const { kmZaKazdyRok, kmZaKazdyMesic } = sumRuns()
+
+  nejlepsiRok.value = ''
+  let maxZaRok = 0
+  for (const rok in kmZaKazdyRok) {
+    if (kmZaKazdyRok[rok] > maxZaRok) {
+      maxZaRok = kmZaKazdyRok[rok]
+      nejlepsiRok.value = rok
+    }
+  }
+  nejlepsiRokKm.value = (maxZaRok / 1000).toFixed(1)
+
+  nejlepsiMesic.value = ''
+  let maxZaMesic = 0
+  for (const mesic in kmZaKazdyMesic) {
+    if (kmZaKazdyMesic[mesic] > maxZaMesic) {
+      maxZaMesic = kmZaKazdyMesic[mesic]
+      nejlepsiMesic.value = mesic
+    }
+  }
+  nejlepsiMesic.value = nejlepsiMesic.value.split('-').reverse().join('/')
+  nejlepsiMesicKm.value = (maxZaMesic / 1000).toFixed(1)
+
+  let celkemZaVsechnyRoky = 0
+  let pocetLet = 0
+  for (const year in kmZaKazdyRok) {
+    celkemZaVsechnyRoky += kmZaKazdyRok[year]
+    pocetLet++
+  }
+  prumerZaRok.value = ((celkemZaVsechnyRoky / pocetLet) / 1000).toFixed(1)
+
+  let celkemZaVsechnyMesice = 0
+  let pocetMesicu = 0
+  for (const mesic in kmZaKazdyMesic) {
+    if (mesic.endsWith(aktualniMesic)) {
+      celkemZaVsechnyMesice += kmZaKazdyMesic[mesic]
+      pocetMesicu++
+    }
+  }
+  prumerZaMesic.value = ((celkemZaVsechnyMesice / pocetMesicu) / 1000).toFixed(1)
+}, { immediate: true })
+
+function sumRuns() {
+  const kmZaKazdyRok = {} as RunStats
+  const kmZaKazdyMesic = {} as RunStats
+  runs.forEach((run) => {
+    const runRok = useDateFormat(run.rdate, 'YYYY').value
+    const runMesic = useDateFormat(run.rdate, 'YYYY-MM').value
+
+    if (!kmZaKazdyRok[runRok]) {
+      kmZaKazdyRok[runRok] = 0
+    }
+    kmZaKazdyRok[runRok] += run.tid > 0 ? run.tlength : run.rlength
+
+    if (!kmZaKazdyMesic[runMesic]) {
+      kmZaKazdyMesic[runMesic] = 0
+    }
+    kmZaKazdyMesic[runMesic] += run.tid > 0 ? run.tlength : run.rlength
+  })
+
+  return {
+    kmZaKazdyRok,
+    kmZaKazdyMesic,
   }
 }
-const nejepsiRokKm = (maxZaRok / 1000).toFixed(1)
-
-let nejlepsiMesic = ''
-let maxZaMesic = 0
-for (const mesic in kmZaKazdyMesic) {
-  if (kmZaKazdyMesic[mesic] > maxZaMesic) {
-    maxZaMesic = kmZaKazdyMesic[mesic]
-    nejlepsiMesic = mesic
-  }
-}
-nejlepsiMesic = nejlepsiMesic.split('-').reverse().join('/')
-const nejlepsiMesicKm = (maxZaMesic / 1000).toFixed(1)
 
 function getCelkemZaObdobi(rok: string, mesic?: string) {
   let totalYear = 0
@@ -108,31 +155,5 @@ function getCelkemZaObdobi(rok: string, mesic?: string) {
     }
   })
   return (totalYear / 1000).toFixed(1)
-}
-
-let celkemZaVsechnyRoky = 0
-let pocetLet = 0
-for (const year in kmZaKazdyRok) {
-  celkemZaVsechnyRoky += kmZaKazdyRok[year]
-  pocetLet++
-}
-const prumerZaRok = ((celkemZaVsechnyRoky / pocetLet) / 1000).toFixed(1)
-
-let celkemZaVsechnyMesice = 0
-let pocetMesicu = 0
-for (const mesic in kmZaKazdyMesic) {
-  if (mesic.endsWith(aktualniMesic)) {
-    celkemZaVsechnyMesice += kmZaKazdyMesic[mesic]
-    pocetMesicu++
-  }
-}
-const prumerZaMesic = ((celkemZaVsechnyMesice / pocetMesicu) / 1000).toFixed(1)
-
-function getCelkem() {
-  let totalMeters = 0
-  runs.forEach((run) => {
-    totalMeters += run.tid > 0 ? run.tlength : run.rlength
-  })
-  return (totalMeters / 1000).toFixed(1)
 }
 </script>
