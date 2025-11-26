c073e22303fe37e960a8c623c02b376e22008963
AloisSeckar <ellrohir@seznam.cz>
2025-11-26 17:50:43 +0100

    feat: better meta tags

--- app/app.vue
+++ app/app.vue
@@ -10,4 +10,14 @@ useHead({
     { name: 'description', content: 'Personal page of Alois Sečkár, Prague, Czechia' },
   ],
 })
+useSeoMeta({
+  title: 'Alois-Seckar.cz',
+  description: 'Alois Sečkár - portfolio',
+  ogType: 'website',
+  ogUrl: 'https://alois-seckar.cz/',
+  ogTitle: 'Alois-Seckar.cz',
+  ogDescription: 'Alois Sečkár - portfolio',
+  ogImage: 'https://master-coda.cz/alois-seckar-cz.jpg',
+  twitterCard: 'summary_large_image',
+})
 </script>
