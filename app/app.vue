65b46895580fafb3859888b669faf4cc70412e21
Alois Sečkár <ellrohir@seznam.cz>
2025-11-27 21:53:32 +0100

    feat: add even more meta tags

--- app/app.vue
+++ app/app.vue
@@ -12,12 +12,26 @@ useHead({
 })
 useSeoMeta({
   title: 'Alois-Seckar.cz',
+  applicationName: 'Alois-Seckar.cz',
   description: 'Alois Sečkár - portfolio',
+  creator: 'Alois Sečkár',
   ogType: 'website',
   ogUrl: 'https://alois-seckar.cz/',
   ogTitle: 'Alois-Seckar.cz',
+  ogSiteName: 'Alois-Seckar.cz',
   ogDescription: 'Alois Sečkár - portfolio',
-  ogImage: 'https://master-coda.cz/alois-seckar-cz.jpg',
+  ogImage: 'https://alois-seckar.cz/alois-seckar-cz.jpg',
+  ogImageAlt: 'Alois Sečkár - website image',
+  ogImageType: 'image/jpeg',
+  ogImageWidth: '1000',
+  ogImageHeight: '525',
   twitterCard: 'summary_large_image',
+  twitterTitle: 'Alois-Seckar.cz',
+  twitterDescription: 'Alois Sečkár - portfolio',
+  twitterImage: 'https://alois-seckar.cz/alois-seckar-cz.jpg',
+  twitterImageAlt: 'Alois Sečkár - website image',
+  twitterImageType: 'image/jpeg',
+  twitterImageWidth: '1000',
+  twitterImageHeight: '525',
 })
 </script>
