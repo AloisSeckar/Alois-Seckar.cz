8a5ebdd833fa577bfb35a3bc8e0f78ca3d8233d8
Alois Sečkár <ellrohir@seznam.cz>
2026-08-24 15:34:16 +0200

    feat: add Nuxt and IT news frontend pages

---
+++ app/pages/digest-it.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ThePageHeader subtitle="IT Digest" />
+    <div id="content" class="content-pane">
+      <p class="text-left sm:text-justify my-4">
+        Sbírka zajímavých článků a&nbsp;aktuálních novinek ze světa IT spravovaná v&nbsp;repozitáři
+        <NuxtLink to="https://github.com/AloisSeckar/master-coda" class="inline-block">
+          AloisSeckar/master-coda
+        </NuxtLink>.
+        Další položky se načítají postupně při scrollování dolů.
+      </p>
+
+      <ArticleDigest :url="CODA_DIGEST_URL" />
+    </div>
+    <ThePageFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+const CODA_DIGEST_URL = 'https://raw.githubusercontent.com/AloisSeckar/master-coda/refs/heads/master/CodaDigest.md'
+
+useSeoMeta({
+  title: 'Coda Digest | Alois-Seckar.cz',
+  description: 'Sbírka zajímavých článků a novinek ze světa IT.',
+})
+</script>
---
+++ app/pages/digest-nuxt.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ThePageHeader subtitle="Nuxt News" />
+    <div id="content" class="content-pane">
+      <p class="text-left sm:text-justify my-4">
+        Sbírka zajímavých článků a&nbsp;aktuálních novinek ze světa frameworku Nuxt a&nbsp;Vue.js spravovaná v&nbsp;repozitáři
+        <NuxtLink to="https://github.com/AloisSeckar/demos-nuxt" class="inline-block">
+          AloisSeckar/demos-nuxt
+        </NuxtLink>.
+        Další položky se načítají postupně při scrollování dolů.
+      </p>
+
+      <ArticleDigest :url="NUXT_NEWS_URL" />
+    </div>
+    <ThePageFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+const NUXT_NEWS_URL = 'https://raw.githubusercontent.com/AloisSeckar/demos-nuxt/refs/heads/main/NuxtNews.md'
+
+useSeoMeta({
+  title: 'Nuxt News | Alois-Seckar.cz',
+  description: 'Sbírka zajímavých článků a novinek ze světa frameworku Nuxt a Vue.js.',
+})
+</script>
