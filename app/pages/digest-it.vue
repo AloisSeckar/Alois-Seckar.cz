a8cfddcc98ef007e63642f3dd1e9a9ccfc1df194
Alois Sečkár <ellrohir@seznam.cz>
2026-08-25 09:38:50 +0200

    feat: navigation between news digest pages

--- app/pages/digest-it.vue
+++ app/pages/digest-it.vue
@@ -3,11 +3,7 @@
     <ThePageHeader subtitle="IT Digest" />
     <div id="content" class="content-pane">
       <p class="text-left sm:text-justify my-4">
-        Sbírka zajímavých článků a&nbsp;aktuálních novinek ze světa IT spravovaná v&nbsp;repozitáři
-        <NuxtLink to="https://github.com/AloisSeckar/master-coda" class="inline-block">
-          AloisSeckar/master-coda
-        </NuxtLink>.
-        Další položky se načítají postupně při scrollování dolů.
+        Sbírka zajímavých článků a&nbsp;aktuálních novinek ze světa IT spravovaná v&nbsp;repozitáři <NuxtLink to="https://github.com/AloisSeckar/master-coda" class="inline-block">AloisSeckar/master-coda</NuxtLink>. Přečíst si můžete i&nbsp;moje <NuxtLink to="/digest-nuxt" class="inline-block">Nuxt News</NuxtLink> a&nbsp;<NuxtLink to="/digest-java" class="inline-block">Java News</NuxtLink>.
       </p>
 
       <ArticleDigest source="coda" />
--- app/pages/digest-java.vue
+++ app/pages/digest-java.vue
@@ -3,12 +3,7 @@
     <ThePageHeader subtitle="Java News" />
     <div id="content" class="content-pane">
       <p class="text-left sm:text-justify my-4">
-        Sbírka zajímavých článků a&nbsp;aktuálních novinek ze světa Javy spravovaná
-        v&nbsp;repozitáři
-        <NuxtLink to="https://github.com/AloisSeckar/demos-java" class="inline-block">
-          AloisSeckar/demos-java
-        </NuxtLink>.
-        Další položky se načítají postupně při scrollování dolů.
+        Sbírka zajímavých článků a&nbsp;aktuálních novinek ze světa Javy spravovaná v&nbsp;repozitáři <NuxtLink to="https://github.com/AloisSeckar/demos-java" class="inline-block">AloisSeckar/demos-java</NuxtLink>. Přečíst si můžete i&nbsp;moje <NuxtLink to="/digest-nuxt" class="inline-block">Nuxt News</NuxtLink> a&nbsp;<NuxtLink to="/digest-it" class="inline-block">IT Digest</NuxtLink>.
       </p>
 
       <ArticleDigest source="java" />
